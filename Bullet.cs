7900194a3ef3be6065884923b213461b90c62fc5
Bozinsky1 <123547155+Bozinsky1@users.noreply.github.com>
2023-05-03 15:25:54 +0200

    Add files via upload

---
+++ Bullet.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Transform target;
+    public float speed = 70f;
+    public float explosionRadius = 0f;
+    public void Seek(Transform _target) // Pour aller chercher la target que j'ai fait dans le code Enemy
+    {
+        target = _target;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+        if (dir.magnitude <= distanceThisFrame) // Permet d'eviter de shoot après que l'ennemi soit passé 
+        {
+            HitTarget();
+            return;
+        }
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World); // On fai bouger la bullet 
+        transform.LookAt(target);
+    }
+    void HitTarget()
+    {
+        if (explosionRadius > 0f)
+        {
+            Explode();
+        } else
+        {
+            Damage(target);
+        }
+        
+        Destroy(gameObject);
+    }
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.tag == "Enemy")
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+}
